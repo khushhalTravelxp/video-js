e0c7e60be6f11311b96699d50e8e5a2fcd2f9a23
KhushhalTravelxp <khushhal.sharma@travelxp.tv>
2025-02-22 15:36:26 +0530

    handled player 5

--- src/app/page.js
+++ src/app/page.js
@@ -38,6 +38,9 @@ export default function ShakaPlayer() {
               }
             }
           }
+        },
+        streaming: {
+          useNativeHlsForFairPlay: false
         }
       });
 
